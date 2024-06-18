5efe4c1f616b2f2fc79edfb73a49ff67f4bf3fb7
Kyra15-CP <kmovva@carepredict.com>
2024-06-18 14:31:40 -0400

    Update tk_GUI.py

--- tk_GUI.py
+++ tk_GUI.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
+from csvfunctions import *
 
 # Data for combobox
-options = active_facility_id()
+options = active_facility_id_name()
 
 # Function to filter combobox options based on input
 def search(event):
