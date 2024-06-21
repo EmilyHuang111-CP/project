fdf4e9e67395649a6b12931fb090ef4a82aa5504
EmilyHuang111-CP <ehuang@carepredict.com>
2024-06-21 15:22:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# Import necessary modules
-from tkinterweb import HtmlFrame  # Assuming tkinterweb is imported correctly
 import tkinter as tk
 from tkinter import ttk, messagebox
-from locator import *
-
-# Function to retrieve active facility IDs and names
-options = active_facility_id_name()
+from locator import active_facility_id_name, match_facility_name, match_institution_name, match_institution_id
+from plotly_graph import *
 
 # Function to filter combobox options based on input
 def search(event):
@@ -29,7 +25,6 @@ def click():
         institution_name_var.set(f"{match_institution_name(selected_value)}")
         institution_id_var.set(f"{match_institution_id(selected_value)}")
 
-
         # Update label2 text
         label2_text.set(f"Facility Name: {facility_name_var.get()}   Facility ID: {selected_value}   Institution Name: {institution_name_var.get()}   Institution ID: {institution_id_var.get()}")
 
@@ -43,6 +38,9 @@ def click():
 root = tk.Tk()
 root.geometry("1000x700")
 root.title("Facility Statistics")
+
+# Retrieve active facility IDs and names
+options = active_facility_id_name()
 
 # Frame 1: Facility Selection
 frame1 = tk.Frame(root)
@@ -59,6 +57,10 @@ drop.pack()
 drop.bind('<KeyRelease>', search)  # Bind KeyRelease event to search function
 drop.bind('<<ComboboxSelected>>', lambda event: var.set(drop.get()))  # Set selected value on selection
 
+# 'Find' Button
+enter_button = tk.Button(frame1, text='Find', command=click)
+enter_button.pack()
+
 # Frame 2: Display Selected Facility Information
 frame2 = tk.Frame(root)
 frame2.pack(pady=20)
@@ -66,20 +68,22 @@ frame2.pack(pady=20)
 facility_name_var = tk.StringVar()
 institution_name_var = tk.StringVar()
 institution_id_var = tk.StringVar()
-
-# 'Find' Button
-enter_button = tk.Button(frame1, text='Find', command=click)
-enter_button.pack()
-
 label2_text = tk.StringVar()
-label2_text.set("")  # Initialize label2 text
 
-label2 = tk.Label(frame1, textvariable=label2_text)
+label2 = tk.Label(frame2, textvariable=label2_text, wraplength=700)
 
-# HTML Browser Frame
-html_frame = HtmlFrame(frame2)
-html_frame.load_website("http://tkhtml.tcl.tk/tkhtml.html")  # Load a sample website
-html_frame.pack(fill="both", expand=True)
+# Create the Plotly graph and get the image buffer
+image_buf = create_plotly_graph()
+
+# Load the image from the buffer
+img = Image.open(image_buf)
+img = ImageTk.PhotoImage(img)
+
+# Create a label to display the image
+image_label = tk.Label(root, image=img)
+image_label.pack(pady=20)
 
 # Start the main tkinter event loop
 root.mainloop()
+
+
